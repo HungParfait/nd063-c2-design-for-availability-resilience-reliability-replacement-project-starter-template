07fb71982e3bce9135fc71f05d797da8b59aafbe
HungParfait <81432965+HungParfait@users.noreply.github.com>
2023-05-22 22:28:36 +0700

    Project 1

---
+++ test-script.sql
@@ -0,0 +1,17 @@
+create table MOCK_DATA (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
+insert into MOCK_DATA (id, first_name, last_name) values (1, 'Danyelle', 'Insull');
+insert into MOCK_DATA (id, first_name, last_name) values (2, 'Vanessa', 'Kinnar');
+insert into MOCK_DATA (id, first_name, last_name) values (3, 'Arleyne', 'Kupka');
+insert into MOCK_DATA (id, first_name, last_name) values (4, 'Goraud', 'Tidbold');
+insert into MOCK_DATA (id, first_name, last_name) values (5, 'Dyann', 'Grigor');
+insert into MOCK_DATA (id, first_name, last_name) values (6, 'Trumann', 'Kirtland');
+insert into MOCK_DATA (id, first_name, last_name) values (7, 'Levi', 'Hoodless');
+insert into MOCK_DATA (id, first_name, last_name) values (8, 'Brockie', 'Erasmus');
+insert into MOCK_DATA (id, first_name, last_name) values (9, 'Claudelle', 'Filippazzo');
+insert into MOCK_DATA (id, first_name, last_name) values (11, 'Kippie', 'Dyhouse');
+
+select * from MOCK_DATA;
